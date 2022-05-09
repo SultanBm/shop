20855f2e616b479849018e6e6b7b111fb9441fb2
sultan beddah <sultanbeddah@gmail.com>
2022-05-09 15:48:00 +0300

    gg

---
+++ shop/src/product.js
@@ -0,0 +1,26 @@
+let products = [
+  {
+    name: "vegetble pizza",
+    price: " $ 2.5",
+    image:
+      "https://img.onmanorama.com/content/dam/mm/en/food/features/images/2021/10/17/pizza.jpg",
+  },
+  {
+    name: "spicy chicken wings",
+    price: "$ 3.5",
+    image:
+      "https://www.thespruceeats.com/thmb/9shc4F8CQ-9alqEnpLNXhWS0WlE=/3068x3068/smart/filters:no_upscale()/traditional-chicken-wings-912937-hero-01-6c1a003373a54538a732abc0005145d8.jpg",
+  },
+];
+
+const productlist = products.map((product) => {
+  return (
+    <div>
+      <img class="pic" src={product.image}></img>
+      <h6 class="word">{product.name}</h6>
+      <h6 class="word">{product.price}</h6>
+    </div>
+  );
+});
+
+export default productlist;
